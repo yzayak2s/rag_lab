a19102fdfae382ace6c204bf0a5ece58486012aa
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2025-01-20 13:48:10 +0100

    created healthCare app file

---
+++ examples/health_care/app.py
@@ -0,0 +1,4 @@
+# references:
+#   https://docs.haystack.deepset.ai/docs/ollamagenerator
+#   https://mer.vin/2024/01/healthcare-chatbot-with-mixtral-8x7b/
+#   original source: https://haystack.deepset.ai/blog/mixtral-8x7b-healthcare-chatbot
