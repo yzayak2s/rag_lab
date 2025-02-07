e1ea22687718d6d43d0acf4471ff70a3ddebc918
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2025-02-07 02:41:07 +0100

    provided simple post http method for generating text with ollama generator

--- src/haystack/app.py
+++ src/haystack/app.py
@@ -1,12 +1,22 @@
 # Importing required libraries
-from flask import Flask
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from haystack_integrations.components.generators.ollama import OllamaGenerator
 
 # Load environment variables from .env and .flaskenv files
 load_dotenv()
 
 app = Flask(__name__)
 
+# Initialize LLM – TODO: Will be outsourced in a separate python-file
+llm = OllamaGenerator(
+    model="llama3.2",
+    url="http://127.0.0.1:11434",
+    generation_kwargs={
+        # "num_predict": 100, # maximum number of tokens to predict when generation text
+        "temperature": 0.0, # creativity – TODO: Will be dynamically adjustable
+    }
+)
 
 @app.route('/')
 def welcome():
@@ -14,3 +24,12 @@ def welcome():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    input_text = request.json.get("text", "")
+    if not input_text:
+        return jsonify({"error": "No input text provided"}), 400
+
+    result = llm.run(input_text)
+    return jsonify({"generated_text": result})
