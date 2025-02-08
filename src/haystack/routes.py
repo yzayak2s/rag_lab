e327e2f8401f2b078be55e702447c3ebcd396b03
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2025-02-08 02:49:14 +0100

    created api/storePDF post endpoint for requesting file information

--- src/haystack/routes.py
+++ src/haystack/routes.py
@@ -1,4 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 # Define the blueprint
 api = Blueprint("api", __name__)
+
+@api.route('/storePDF', methods=['POST'])
+def store_pdf():
+    file = request.json.get("file")
+    if not file:
+        return jsonify({"error": "No file information provided"}), 400
+
+    return {}
