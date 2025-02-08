cee5bd7dc29e1cb7b73ba7eef43be866474dfb51
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2025-02-08 15:05:50 +0100

    refactoring: used qdrant document store in routes.py, renamed splitter to document_splitter, renamed store_vectorized_documents to create_vectorized_documents and added to it vdb parameter

--- src/haystack/routes.py
+++ src/haystack/routes.py
@@ -3,10 +3,12 @@ from flask import Blueprint, request, jsonify
 from haystack.components.preprocessors import DocumentCleaner, DocumentSplitter
 from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbedder
 
-from services.pdf_service import convert_pdf_to_document, store_vectorized_documents
+from services.pdf_service import convert_pdf_to_document, create_vectorized_documents
+from qdrant_store import get_qdrant_document_store
 
 # Define the blueprint
 api = Blueprint("api", __name__)
+qdrant_document_store = get_qdrant_document_store()
 
 ollama_model = dotenv_values(find_dotenv(".flaskenv")).get('OLLAMA_MODEL')
 ollama_url = dotenv_values(find_dotenv(".flaskenv")).get('OLLAMA_URL')
@@ -20,9 +22,9 @@ def store_pdf():
     documents = convert_pdf_to_document(file["file_path"], file["authors"])
     document_cleaner = DocumentCleaner(remove_repeated_substrings=True)
     cleaned_documents = document_cleaner.run(documents=documents)
-    splitter = DocumentSplitter(split_by="word", split_length=400)
-    split_documents = splitter.run(documents=cleaned_documents['documents'])
+    document_splitter = DocumentSplitter(split_by="word", split_length=400)
+    split_documents = document_splitter.run(documents=cleaned_documents['documents'])
     document_embedder = OllamaDocumentEmbedder(model=ollama_model, url=ollama_url)
     vectorized_documents = document_embedder.run(documents=split_documents['documents'])
 
-    return store_vectorized_documents(vectorized_documents['documents'])
+    return create_vectorized_documents(qdrant_document_store, vectorized_documents['documents'])
--- src/haystack/services/pdf_service.py
+++ src/haystack/services/pdf_service.py
@@ -1,6 +1,4 @@
 from haystack.components.converters import PyPDFToDocument
-from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
-from haystack.document_stores.types import DuplicatePolicy
 
 import logging
 
@@ -24,21 +22,13 @@ def convert_pdf_to_document(file_path: str, authors: str):
 
     return results["documents"]
 
-def store_vectorized_documents(vectorized_documents):
+def create_vectorized_documents(vdb, vectorized_documents):
     """
     This function stores vectorized documents in Qdrant document store.
     :return:
     """
-    # Initialize QdrantDocumentStore with the specified path
-    document_store = QdrantDocumentStore(
-        path="qdrant/storage_local",
-        index="Document",
-        embedding_dim=3072,
-        # recreate_index=True,
-        # hnsw_config={"m": 16, "ef_construct": 64} # Optional
-    )
 
     try:
-        return {"count": document_store.write_documents(documents=vectorized_documents, policy=DuplicatePolicy.SKIP)}
+        return {"count": vdb.write_documents(documents=vectorized_documents, policy=DuplicatePolicy.SKIP)}
     except Exception as e:
         logger.error(f"Failed to write documents to Qdrant document store: {e}")
