4329e2550fca7737ec7af1b2d47eced57f880596
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2025-02-23 22:39:27 +0100

    refactored document_store.py

--- src/document_store.py
+++ src/document_store.py
@@ -1,13 +1,15 @@
 from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
 
-# Initialize QdrantDocumentStore with the specified path
-qdrant_document_store = QdrantDocumentStore(
-    path="qdrant/storage_local",
-    index="Document",
-    embedding_dim=768, # it differs from model to model
-    recreate_index=True,
-    hnsw_config={"m": 64, "ef_construct": 512}, # Optional
-)
-
 def get_document_store():
-    return qdrant_document_store
+    """
+    Returns the initialized document store.
+
+    :return:
+    """
+    return QdrantDocumentStore(
+        path="qdrant/storage_local",
+        index="Document",
+        embedding_dim=768, # it differs from model to model
+        recreate_index=False,
+        hnsw_config={"m": 64, "ef_construct": 512}, # Optional
+    )
